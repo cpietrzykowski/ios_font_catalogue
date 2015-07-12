ba65667957c876f22f8392f60aafaaf23fdaf5c2
Christopher Pietrzykowski <cpietrzykowski@gmail.com>
2015-07-12 03:19:55 -0600

    add Fonts controller

---
+++ README.md
@@ -0,0 +1,14 @@
+# Font Catalogue
+
+Display available fonts.
+
+## TODO
+  * font details
+  * sample text
+
+## Reference
+  * [UIFont](https://developer.apple.com/library/ios/documentation/UIKit/Reference/UIFont_Class/)
+  * [UIFontDescriptor](https://developer.apple.com/library/ios/documentation/UIKit/Reference/UIFontDescriptor_Class/)
+
+## License
+This work is licensed under a [Creative Commons Attribution-ShareAlike 4.0 International License](http://creativecommons.org/licenses/by-sa/4.0/).
---
+++ iosFontCatalogue/FontsTableViewController.swift
@@ -0,0 +1,124 @@
+//
+//  FontsTableViewController.swift
+//  iosFontCatalogue
+//
+//  Created by Christopher Pietrzykowski on 2015-07-12.
+//  Copyright (c) 2015 Zykix. All rights reserved.
+//
+
+import UIKit
+
+class FontsTableViewController: UITableViewController {
+
+    var _fonts: [String: [String]] = [String: [String]]()
+    var _sectionTitles: [String] = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        // load fonts to display
+        let fontFamilyNames = UIFont.familyNames()
+        for familyName in fontFamilyNames {
+            var key = familyName as! String
+            self._fonts[key] = [String]()
+            let names = UIFont.fontNamesForFamilyName(familyName as! String) as! [String]
+            self._fonts[key] = names
+        }
+        
+        // add and sort section titles
+        self._sectionTitles = self._fonts.keys.array.sorted({
+            $0 < $1 // implicit block return
+        })
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return self._sectionTitles.count
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return self._fonts[self._sectionTitles[section]]!.count
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self._sectionTitles[section]
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("FontNameCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        // Configure the cell...
+        var fontName = self._fonts[self._sectionTitles[indexPath.section]]![indexPath.row]
+        cell.textLabel?.text = fontName
+        
+        dispatch_async(dispatch_get_main_queue(), {
+            var labelFont = UIFontDescriptor(name: fontName, size: 18.0)
+            cell.textLabel?.font = UIFont(descriptor: labelFont, size: 0.0)
+        })
+        
+        return cell
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
